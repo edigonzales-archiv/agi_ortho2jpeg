18ef8a85ac387acd75e0bbce92e1168c4e21a39d
Stefan Ziegler <edi.gonzales@gmail.com>
2016-04-09 18:20:13 +0200

    leichte parametrisierung

--- raster2jpeg.py
+++ raster2jpeg.py
@@ -5,20 +5,44 @@ from osgeo import ogr
 from osgeo import osr
 from osgeo import gdal
 import os
+import os.path
 import sys
 
+# path to gdal devel
+GDAL_PATH = "/usr/local/gdal_master/bin/"
+
+# set input and output path
+img_path = sys.argv[1]
+if img_path[0] == "/":
+    img_path = img_path[1:]
+    
+INPUT_PATH = os.path.join("/home/stefan/mnt/NAS-DS215j/Geodaten/", img_path) 
+OUTPUT_PATH = os.path.join("/home/stefan/tmp/", img_path)
+
+# throw exceptions
 gdal.UseExceptions()
 ogr.UseExceptions()
 
+# check gdal version
 version_num = int(gdal.VersionInfo('VERSION_NUM'))
 print version_num
 
-GDAL_PATH = "/usr/local/gdal_master/bin/"
-INPUT_PATH = "/home/stefan/mnt/NAS-DS215j/tmp/2012/"
-OUTPUT_PATH = "/home/stefan/tmp/2012/"
 
-gpkg = ogr.Open(os.path.join(INPUT_PATH,"ortho2012_idx.gpkg"))
-layer = gpkg.GetLayerByName("ortho2012_idx")
+# create output path
+try:
+    os.makedirs(OUTPUT_PATH)
+except Exception:
+    pass
+    
+# create index gpkg
+raster_idx = "raster_idx.gpkg"
+
+cmd = "gdaltindex -f GPKG -t_srs EPSG:21781  " + OUTPUT_PATH + "/" + raster_idx + " " + INPUT_PATH + "/*.tif"
+os.system(cmd)
+
+# loop over index gpkg
+gpkg = ogr.Open(os.path.join(OUTPUT_PATH, raster_idx))
+layer = gpkg.GetLayer(0)
 
 for feature in layer:
     infileName = feature.GetField('location')
--- raster2jpeg.sh
+++ raster2jpeg.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gdal_master/lib
 export PYTHONPATH=$PYTHONPATH:/usr/local/gdal_master/lib/python2.7/site-packages/
 
-./raster2jpeg.py
+./raster2jpeg.py $1
